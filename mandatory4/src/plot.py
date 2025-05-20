34896604b08c68f4e3415de24206339e732e0286
August Femtehjell <august.femtehjell@gmail.com>
2025-05-20 14:28:17 +0200

    Start exam document

--- mandatory4/src/plot.py
+++ mandatory4/src/plot.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from data import retrieve_dataset
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def plot_3d_points(
-    data: np.ndarray | list[np.ndarray], title: str = "3D Curve", line: bool = False
-) -> None:
+    data: np.ndarray | list[np.ndarray],
+    title: str = "3D Curve",
+    line: bool = False,
+    ax: Axes3D | None = None,
+):
     """Plot 3D points.
 
     Args:
         data (np.ndarray): The input array of shape (x_i, y_i, z_i), for i = 1, ..., n.
         title (str): The title of the plot.
     """
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
+    given_ax = ax is not None
+    if not given_ax:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
 
     if isinstance(data, np.ndarray):
         data = [data]
@@ -24,9 +30,13 @@ def plot_3d_points(
         func(d[:, 0], d[:, 1], d[:, 2])
 
     ax.set_title(title)
-    ax.set_xlabel("X-axis")
-    ax.set_ylabel("Y-axis")
-    ax.set_zlabel("Z-axis")
+    ax.set_xlabel("$X$")
+    ax.set_ylabel("$Y$")
+    ax.set_zlabel("$Z$")
+
+    if given_ax:
+        return ax
+
     plt.show()
 
 
--- mandatory4/src/solve_system.py
+++ mandatory4/src/solve_system.py
@@ -1,32 +1,26 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from data import chord_length_parametrization, retrieve_dataset
-from spline_eval import eval_bspline_at_point, eval_spline
+from mpl_toolkits.mplot3d import Axes3D
+from plot import plot_3d_points
+from spline_eval import eval_bspline_at_point
 
 
 def setup_A(u_arr: np.ndarray, knot_percentage: float) -> tuple[np.ndarray, np.ndarray]:
     if not 0 < knot_percentage <= 1:
         raise ValueError("knot_percentage must be in (0, 1])")
 
-    # data = retrieve_dataset(file_nr)
     m = u_arr.shape[0]
     n = int(m * knot_percentage)
     d = 3
-    # if knot_percentage == 1:
-    #     n = m - d - 1  # First and last knot are the same
 
-    # u_arr = chord_length_parametrization(data)
     u_min = u_arr[0]
     u_max = u_arr[-1]
-    # knot_vector = np.linspace(u_min, u_max, n + d + 1)
+
     knot_start = np.repeat(u_min, d)
     knot_mid = np.linspace(u_min, u_max, n - d + 1)
     knot_end = np.repeat(u_max, d)
     knot_vector = np.concatenate((knot_start, knot_mid, knot_end))
-
-    """
-    (n + d + 1) - 2d
-    = n - d + 1
-    """
 
     A = np.zeros((m, n))
     for i in range(m):
@@ -52,55 +46,59 @@ def compute_coeffs(
     u_arr = chord_length_parametrization(data)
     A, knot_vector = setup_A(u_arr, knot_percentage)
 
-    ATA_inv = np.linalg.pinv(A.T @ A)
-    coeffs = ATA_inv @ A.T @ data
+    # ATA_inv = np.linalg.inv(A.T @ A)
+    # coeffs = ATA_inv @ A.T @ data
+
+    coeffs = np.linalg.lstsq(A, data)[0]
+
     return coeffs, knot_vector
 
 
-if __name__ == "__main__":
-    layers = []
-    d = 3
-    # file_nr = 2
-    knot_percentage = 0.2
+def solve_and_plot(knot_percentage: float, ax: Axes3D) -> None:
+    """Solve the B-spline system.
 
+    Args:
+        knot_percentage (float): The percentage of knots to use.
+        ax (Axes3D): The Axes3D object to plot on. Defaults to None.
+    """
+    layers = []
+    degree = 3
     for file_nr in range(1, 10):
         coeffs, knot_vector = compute_coeffs(file_nr, knot_percentage)
-        n = len(knot_vector) - d - 1
+        n = len(knot_vector) - degree - 1
 
         u_vals = np.linspace(knot_vector[0], knot_vector[-1], 200)
-        print(knot_vector)
         b_spline_vals = np.array(
             [
-                eval_bspline_at_point(i, 3, knot_vector, u)
+                eval_bspline_at_point(i, degree, knot_vector, u)
                 for u in u_vals
                 for i in range(n)
             ]
         ).reshape(len(u_vals), -1)
 
-        results = b_spline_vals @ coeffs  # [10:-10]
+        results = b_spline_vals @ coeffs
         layers.append(results)
 
-    from plot import plot_3d_points
+    plot_3d_points(layers, title=f"With ${knot_percentage * 100}$%", line=True, ax=ax)
 
-    plot_3d_points(layers, title="3D Curve of the heart", line=True)
 
-    # print(b_spline_vals[0:10])
-    # print(b_spline_vals[-10:])
-    # print(coeffs[0])
-    # print(b_spline_vals[0])
+if __name__ == "__main__":
+    import time
 
-    # print(results[:, 2])
-    # import matplotlib.pyplot as plt
+    knot_percentages = [0.05, 0.1, 0.2]
+    fig = plt.figure()
+    axs = fig.subplots(1, len(knot_percentages), subplot_kw={"projection": "3d"})
 
-    # plt.plot(results[:, 0], results[:, 1], label="B-spline curve")
-    # plt.scatter(coeffs[:, 0], coeffs[:, 1], color="red", label="Control Points", s=10)
-    # data = retrieve_dataset(file_nr)
-    # plt.scatter(data[:, 0], data[:, 1], color="green", label="Data Points", s=10)
-    # plt.legend()
-    # plt.title("B-spline curve")
-    # plt.xlabel("X-axis")
-    # plt.ylabel("Y-axis")
-    # plt.show()
-    # results = np.array([eval_spline(coeffs, u_arr, u) for u in u_vals])
-    # plt.plot(results[:, 0], results[:, 1])
-    # plt.show()
+    start = time.perf_counter()
+
+    for i, knot_percentage in enumerate(knot_percentages):
+        ax = axs[i]
+        solve_and_plot(knot_percentage, ax=ax)
+
+    end = time.perf_counter()
+    print(f"Time taken: {end - start:.2f} seconds")
+
+    fig.suptitle("B-spline with different knot percentages")
+    fig.tight_layout()
+
+    plt.show()
